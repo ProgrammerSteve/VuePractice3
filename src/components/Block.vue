8af5419ce52c6435d12ac134bd387d494bee23f5
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-08-15 17:23:41 -0500

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="block" v-if="showBlock">click me</div>
+</template>
+<script>
+export default {
+  data(){
+return {
+  showBlock:false,
+}
+  },
+  props:["delay"],
+  mounted(){
+    console.log("component mounted")
+    setTimeout(() => {
+      this.showBlock=true
+    }, this.delay);
+  },
+  updated(){
+console.log("Component Updated:",this.showBlock)
+  },
+  unmounted(){
+    console.log("unmounted")
+  }
+}</script>
+<style>
+  .block{
+    width:400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding:100px 0;
+    margin:40px auto;
+
+  }
+</style>
